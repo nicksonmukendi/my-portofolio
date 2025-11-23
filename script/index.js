53d9fde71db4bfdde1bd6a775c48c36b2221f652
nicksonmb <nixyap@gmail.com>
2025-11-23 13:47:35 +0100

    index.html

---
+++ script/index.js
@@ -0,0 +1,7 @@
+// get new year updated
+const currentyear = new Date().getFullYear();
+document.getElementById('currentyear').textContent= new Date().getFullYear();
+
+// get lastupdated
+
+document.getElementById('lastupdated'). textContent= document.lastModified;
